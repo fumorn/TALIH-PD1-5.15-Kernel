dc587bf109784d6e80e1bb7fd26686afa7b242ac
fumorn <fumor@foxmail.com>
2026-08-23 12:01:10 +0800

    debug: bypass X.509 cert loading (abort in key_create_or_update/ASN.1)

--- certs/system_keyring.c
+++ certs/system_keyring.c
@@ -149,20 +149,9 @@ __init int load_module_cert(struct key *keyring)
  */
 static __init int load_system_certificate_list(void)
 {
-	const u8 *p;
-	unsigned long size;
-
 	pr_notice("Loading compiled-in X.509 certificates\n");
-
-#ifdef CONFIG_MODULE_SIG
-	p = system_certificate_list;
-	size = system_certificate_list_size;
-#else
-	p = system_certificate_list + module_cert_size;
-	size = system_certificate_list_size - module_cert_size;
-#endif
-
-	return load_certificate_list(p, size, builtin_trusted_keys);
+	pr_notice("DBG-X509: bypass cert loading (temporary)\n");
+	return 0;
 }
 late_initcall(load_system_certificate_list);
 
